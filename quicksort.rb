788c314667146f2b4d02ce20412af9f4d657fe96
Douglas Gordon <douglastgordon@gmail.com>
2016-11-17 11:50:42 -0500

    remove print statement

--- quicksort.rb
+++ quicksort.rb
@@ -1,10 +1,10 @@
+# Time Complexity O(n log n)
 def quicksort(arr)
   return arr if arr.length <= 1
   pivot = arr[arr.length/2]
   arr.delete_at(arr.length/2)
   left = []
   right = []
-  p pivot
 
   arr.each do |el|
     if el <= pivot
@@ -19,3 +19,5 @@ def quicksort(arr)
 
   left.concat([pivot]).concat(right)
 end
+
+p quicksort([1,2,3,4,5,6,7,7,8,9])
