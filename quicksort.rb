ed60af4552aed695200bc8fa061ac0b17aeef7d6
Douglas Gordon <douglastgordon@gmail.com>
2016-11-17 11:34:34 -0500

    quicksort

---
+++ quicksort.rb
@@ -0,0 +1,21 @@
+def quicksort(arr)
+  return arr if arr.length <= 1
+  pivot = arr[arr.length/2]
+  arr.delete_at(arr.length/2)
+  left = []
+  right = []
+  p pivot
+
+  arr.each do |el|
+    if el <= pivot
+      left << el
+    else
+      right << el
+    end
+  end
+
+  left = quicksort(left)
+  right = quicksort(right)
+
+  left.concat([pivot]).concat(right)
+end
